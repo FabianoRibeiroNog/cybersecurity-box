9ee4c46f06faa9b1884cbab2c82180f6559c5f44
Fabiano Ribeiro <fabianoribeironog@gmail.com>
2026-08-23 18:02:07 -0300

    feat: add continuous wi-fi monitoring loop

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import time
 import wifi_monitor
 
+INTERVALO_SCAN = 300
 
 def main():
     print("Cybersecurity Box iniciada!")
     print("Iniciando Wi-Fi Monitor...")
     print()
 
-    redes = wifi_monitor.escanear_redes()
+    while(True):
+        redes = wifi_monitor.escanear_redes()
 
-    print("Redes encontradas:", len(redes))
-    print()
+        print("Redes encontradas:", len(redes))
+        print()
 
-    for rede in redes:
-        wifi_monitor.exibir_rede(rede)
+        for rede in redes:
+            wifi_monitor.exibir_rede(rede)
 
+            print()
+            print("Novo scan em", INTERVALO_SCAN, "Segundos... ")
+            print()
+
+            time.sleep(INTERVALO_SCAN)
 
 main()
