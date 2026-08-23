570da3ef2186fba9d74e609b78c76f92977c4426
Fabiano Ribeiro <fabianoribeironog@gmail.com>
2026-08-23 15:31:22 -0300

    feat: add initial ESP32 wi-fi network monitor

---
+++ main.py
@@ -0,0 +1,22 @@
+import network
+
+print("Cybersecurity Box iniciada!")
+print("Iniciando Wi-fi Monitor...")
+
+wlan = network.WLAN(network.WLAN.IF_STA)
+wlan.active(True)
+
+redes = wlan.scan()
+
+print("Redes encontradas: ", len(redes))
+print()
+
+for rede in redes:
+    ssid = rede[0].decode("utf-8")
+    canal = rede[2]
+    rssi = rede[3]
+
+    print("SSID: ", ssid)
+    print("CANAL: ", canal)
+    print("RSSI", rssi)
+    print("-------------------")
