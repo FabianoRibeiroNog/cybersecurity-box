243b23445f989feb4ad84653a73566e4dfea478e
Fabiano Ribeiro <fabianoribeironog@gmail.com>
2026-08-23 16:12:16 -0300

    feat: add wi-fi security information

--- main.py
+++ main.py
@@ -12,14 +12,26 @@ redes = wlan.scan()
 print("Redes encontradas: ", len(redes))
 print()
 
+tiposSeguranca = {
+    0: "OPEN",
+    1: "WEP",
+    2: "WPA-PSK",
+    3: "WPA2-PSK",
+    4: "WPA/WPA2-PSK"
+}
+
 for rede in redes:
     ssid = rede[0].decode("utf-8")
     bssid = binascii.hexlify(rede[1], ":").decode("utf-8")
     canal = rede[2]
     rssi = rede[3]
+    codigo_seguranca = rede[4]
+
+    seguranca = tiposSeguranca.get(codigo_seguranca, "DESCONHECIDA")
 
     print("SSID: ", ssid)
     print("BSSID: ", bssid)
     print("CANAL: ", canal)
     print("RSSI", rssi)
+    print("SEGURANCA: ", seguranca)
     print("-------------------")
